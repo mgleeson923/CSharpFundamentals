fecf74151b07a9469e38bfc88afc8a90c684676e
Michael Gleeson <mgleeson923@gmail.com>
2019-07-18 15:58:46 -0400

    Arrays and Variables

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,15 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new[] { 12.7, 18.9, 9.7, 4.1 };
+
+            var result = numbers[0];
+            result += numbers[1];
+            result += numbers[2];
+
+            Console.WriteLine(result);
+
+
 
             if (args.Length > 0)
             {
