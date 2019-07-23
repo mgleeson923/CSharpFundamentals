56ec351059d72c59122e53658bfeb5b6dadd5af9
Michael Gleeson <mgleeson923@gmail.com>
2019-07-23 15:58:26 -0400

    Added Unit Tests

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -16,6 +16,25 @@ namespace GradeBook
             grades.Add(grade);
         }
 
-        List<double> grades;
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            Console.WriteLine($"The average grade is {result:N1}");
+            }
+
+        private List<double> grades;
+        private string name; 
     }
 }
