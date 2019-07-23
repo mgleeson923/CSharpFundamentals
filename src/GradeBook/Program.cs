a04a17be4bb3f3d1d2aae7abbe92ed089b81b8b2
Michael Gleeson <mgleeson923@gmail.com>
2019-07-23 13:35:54 -0400

    Classes and Objects Final

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,21 +10,26 @@ namespace GradeBook
 
             var book = new Book();
             book.AddGrade(89.1);
-
-            var book2 = new Book();
-            book2.AddGrade(90.1);
-
+            book.AddGrade(90.1);
+            book.AddGrade(77.5);
 
             var grades = new List<double>() { 12.7, 18.9, 9.7, 4.1 };
             grades.Add(56.1);
             
 
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
             foreach (double number in grades)
             {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, highGrade);
                 result += number;
             }
             result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
             Console.WriteLine($"The average grade is {result:N1}");
         }
     }
